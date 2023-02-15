4b4f33768597e22bde5e1632cf331c1660611d4d
kmille <github@androidloves.me>
2023-02-15 11:38:59 +0100

    basically a rewrite

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-	"image/color"
+	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -11,93 +11,114 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
-func get_signal_executable() (error, string) {
+var signal_bin string
+
+const version = "0.1.0"
+
+func chooseSignalBinary(w fyne.Window) {
+	callback := func(uri fyne.URIReadCloser, err error) {
+		if uri == nil {
+			dialog.ShowInformation("Error", "No file selected", w)
+		} else {
+			signal_bin = uri.URI().Path()
+		}
+	}
+	dialogOpen := dialog.NewFileOpen(callback, w)
+	dialogOpen.Show()
+	// call it later, then it will be showed on the top
+	dialog.ShowInformation("Error", "Signal application not found. Please specify", w)
+}
+
+func find_signal_executable(w fyne.Window) {
+	var location string
+	var err error
+
 	switch runtime.GOOS {
 	case "linux":
-		return nil, "signal-desktop"
-	case "darwin":
-		return nil, "/Applications/Signal.app/Contents/MacOS/Signal"
-	case "windows":
-		dir, err := os.UserCacheDir()
+		location, err = exec.LookPath("signal-desktop")
 		if err != nil {
-			return err, ""
+			chooseSignalBinary(w)
+			return
 		}
-		return nil, filepath.Join(dir, "Programs", "signal-desktop", "Signal.exe")
-	default:
-		fmt.Println("OS is not supported")
-		os.Exit(1)
+	case "darwin":
+		location = "/Applications/Signal.app/Contents/MacOS/Signal"
+	case "windows":
+		cache_dir, err := os.UserCacheDir()
+		if err != nil {
+			chooseSignalBinary(w)
+			return
+		}
+		location = filepath.Join(cache_dir, "Programs", "signal-desktop", "Signal.exe")
 	}
-	return errors.New("This is will never be reached"), ""
 
+	if _, err := os.Stat(location); errors.Is(err, fs.ErrNotExist) {
+		chooseSignalBinary(w)
+		return
+	}
+	signal_bin = location
 }
 
-func get_data_dir(account_id int) (error, string) {
+func get_data_dir(account_id int) (string, error) {
 	config_dir, err := os.UserConfigDir()
 	if err != nil {
-		return err, ""
+		return "", err
 	}
 	data_dir := filepath.Join(config_dir, fmt.Sprintf("Signal-Account-%d", account_id))
-	if _, err := os.Stat(data_dir); os.IsNotExist(err) {
+	if _, err := os.Stat(data_dir); errors.Is(err, fs.ErrNotExist) {
 		if err := os.Mkdir(data_dir, os.ModePerm); err != nil {
-			return err, ""
+			return "", err
 		}
 	}
-	return nil, data_dir
+	return data_dir, nil
 }
 
-func change_text(debug *canvas.Text, text string) {
-	debug.Text = text
-	debug.Refresh()
-}
+func run_signal(account_id int, w fyne.Window) {
+	data_dir, err := get_data_dir(account_id)
+	if err != nil {
+		dialog.ShowInformation("Error", fmt.Sprintf("Error creating data_dir: %s", err), w)
+		return
+	}
+	dialog.ShowInformation("Info", fmt.Sprintf("Starting Signal Account #r%d with data_dir %q. Plase wait... ", account_id, data_dir), w)
 
-func run_signal(account_id int, debug *canvas.Text) {
-	err, signal_bin := get_signal_executable()
-	if err != nil {
-		change_text(debug, fmt.Sprintf("Error getting signal binary: %s", err))
-		return
-	}
-	err, data_dir := get_data_dir(account_id)
-	if err != nil {
-		change_text(debug, fmt.Sprintf("Error creating data_dir: %s", err))
-		return
-	}
 	cmd := exec.Command(signal_bin, "--user-data-dir="+data_dir)
-	change_text(debug, fmt.Sprintf("Starting Signal Account %d with data_dir %q. Plase wait... ", account_id, data_dir))
 	err = cmd.Run()
 	if err != nil {
-		change_text(debug, fmt.Sprint("Error executing Signal: %s\n", err))
+		dialog.ShowInformation("Error", fmt.Sprintf("Error executing Signal: %s\n", err), w)
+		return
 	}
 }
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Signal account switcher")
-	w.Resize(fyne.NewSize(400, 500))
-	w.RequestFocus()
+	app := app.New()
+	app.Settings().SetTheme(theme.DarkTheme())
 
-	//debug := canvas.NewText("Hi, please choose account", color.Black)
-	debug := canvas.NewText("Hi, please choose account", color.RGBA{R: 0x32, G: 0xCD, B: 0x32, A: 100})
-	account1 := widget.NewButton("Account #1", func() {
-		run_signal(1, debug)
+	w := app.NewWindow(fmt.Sprintf("Signal account switcher v%s", version))
+	w.Resize(fyne.NewSize(600, 800))
+	find_signal_executable(w)
+
+	account1 := widget.NewButton("Start signal account #1", func() {
+		go run_signal(1, w)
 	})
-	account2 := widget.NewButton("Account #2", func() {
-		run_signal(2, debug)
+	account2 := widget.NewButton("Start signal account #2", func() {
+		go run_signal(2, w)
 	})
-	account3 := widget.NewButton("Account #3", func() {
-		run_signal(3, debug)
+	account3 := widget.NewButton("Start signal account #3", func() {
+		go run_signal(3, w)
 	})
-	account4 := widget.NewButton("Account #4", func() {
-		run_signal(4, debug)
+	account4 := widget.NewButton("Start signal account #4", func() {
+		go run_signal(4, w)
 	})
 
-	grid := container.New(layout.NewGridLayout(1), debug, account1, account2, account3, account4)
+	grid := container.New(layout.NewGridLayout(1), account1, account2, account3, account4)
 	w.SetContent(grid)
+	w.Show()
 
 	w.ShowAndRun()
 	fmt.Println("Exiting ...")
